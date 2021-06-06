b5ebebd785503e2399c5ca41c3b3bb7e2def7215
saikatsingh <84813706+saikatsingh@users.noreply.github.com>
2021-06-06 14:26:47 +0530

    Update force.py

--- plugins/force.py
+++ plugins/force.py
@@ -56,12 +56,12 @@ async def text(bot, update):
                         InlineKeyboardButton("Help", callback_data = "ghelp")
                 ],
                 [
-                    InlineKeyboardButton('Support Channel', url='https://t.me/Mai_bOTs'),
-                    InlineKeyboardButton('Feedback', url='https://t.me/No_OnE_Kn0wS_Me')
+                    InlineKeyboardButton('Support Channel', url='https://t.me/movizenx'),
+                    InlineKeyboardButton('Feedback', url='https://t.me/Movizenix')
                 ],
                 [
-                    InlineKeyboardButton('Other Bots', url='https://t.me/Mai_bOTs/17'),
-                    InlineKeyboardButton('Source', url='https://github.com/No-OnE-Kn0wS-Me/FileRenameBot')
+                    InlineKeyboardButton('Other Bots', url='https://t.me/mxPublicFilesBot'),
+                    InlineKeyboardButton('Owner', url='https://t.me/AnnihilusOP)
                 ]
             ]
         ),
